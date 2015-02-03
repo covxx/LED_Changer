6556488adeb2c93d80e69ecaedd81539b14245ff
covxx <covxx@users.noreply.github.com>
2015-02-03 08:37:34 -0500

    Changed variable
    Changed led to LED

--- code.ino
+++ code.ino
@@ -1,13 +1,14 @@
 //LED_Cube_Code @covxx
+//This isn't optimized for multible LEDs, that needs to be done
 
-int led=13; //makes led a variable for pin 13
+int lED=13; //makes led a variable for pin 13
 
 void setup() {
-	pinMode (led, OUTPUT);
+	pinMode (LED, OUTPUT);
 }
 
 void loop () {
-	digitalWrite(led, HIGH); //sets pin13 voltage 5v (turns on the led)
-	delay(2000); //delay in ms (2 seconds)
-	digitalWrite(led, LOW);  //sets pin voltage to 0
+	digitalWrite(LED, HIGH); //sets pin13 voltage 5v (turns on the LED)
+	delay(2000); //odelay in ms (2 seconds)
+	digitalWrite(LED, LOW);  //sets pin voltage to 0(turns off LED)
 }
