d249b329f68b8d9dca031f010f532ca7800fdd8e
covxx <covxx@users.noreply.github.com>
2015-02-03 09:17:50 -0500

    fixed variable issue

--- code.ino
+++ code.ino
@@ -1,14 +1,15 @@
 //LED_Cube_Code @covxx
 //This isn't optimized for multible LEDs, that needs to be done
 
-int lED=13; //makes led a variable for pin 13
+int led=13; //makes led a variable for pin 13
 
 void setup() {
-	pinMode (LED, OUTPUT);
+	pinMode (led, OUTPUT);
 }
 
 void loop () {
-	digitalWrite(LED, HIGH); //sets pin13 voltage 5v (turns on the LED)
-	delay(2000); //odelay in ms (2 seconds)
-	digitalWrite(LED, LOW);  //sets pin voltage to 0(turns off LED)
+	digitalWrite(led, HIGH); //sets pin13 voltage 5v (turns on the LED)
+	delay(2000); //delay in ms (2 seconds)
+	digitalWrite(led, LOW);  //sets pin voltage to 0(turns off LED)
+        delay(2000);
 }
